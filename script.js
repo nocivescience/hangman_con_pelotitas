3af08fc4b0ac23f5170a4bf30ac2760051224f69
noceive <nocivescience@gmail.com>
2022-11-13 18:38:17 -0300

    comenezando de cero

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const cardContainer=document.getElementById('cardContainer');
 const cards=document.querySelectorAll('.card-body');
 const listWords=['hola','adios','como','estas']
-const wordSelected=listWords[Math.floor(Math.random()*listWords.length)]
-function addCardsChance(n){
-    for(let i=0;i<n;i++){
-        const card=document.createElement('div');
-        card.innerHTML=`
-            <div class="card-chance">${i}</div>
-        `;
-        cards[0].appendChild(card)
-    }
-}
-addCardsChance(5);
+const wordSelected=listWords[Math.floor(Math.random()*listWords.length)];
+let correctLetters=[];
+let wrongLetters=[];
